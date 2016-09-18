a741ba78327006fc6446daf08b7fdd8630504874
James Craig <admin@gutgames.com>
2016-09-18 17:38:53 -0400

    Copied over the basic tests from ImageProcessorCore. At this point just basic optimization of each class is what is needed.

---
+++ Structure.Sketching.Benchmarks/GenericSpeedTests/ArrayCopyTests.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Structure.Sketching.Benchmarks.GenericSpeedTests
+{
+    public class ArrayCopyTests
+    {
+        [Params(100, 1000, 10000)]
+        public int Count { get; set; }
+
+        private long sizeInBytes;
+        private byte[] source, destination;
+
+        [Benchmark(Baseline = true, Description = "Copy using Array.Copy()")]
+        public void CopyArray()
+        {
+            Array.Copy(source, destination, Count);
+        }
+
+        [Benchmark(Description = "Copy using Unsafe<T>")]
+        public unsafe void CopyUnsafe()
+        {
+            fixed (byte* pinnedDestination = destination)
+            fixed (byte* pinnedSource = source)
+            {
+                Unsafe.CopyBlock(pinnedSource, pinnedDestination, (uint)Count);
+            }
+        }
+
+        [Benchmark(Description = "Copy using Buffer.MemoryCopy<T>")]
+        public unsafe void CopyUsingBufferMemoryCopy()
+        {
+            fixed (byte* pinnedDestination = destination)
+            fixed (byte* pinnedSource = source)
+            {
+                Buffer.MemoryCopy(pinnedSource, pinnedDestination, Count, Count);
+            }
+        }
+
+        [Setup]
+        public void SetUp()
+        {
+            source = new byte[Count];
+            destination = new byte[Count];
+        }
+    }
+}
---
+++ Structure.Sketching.Benchmarks/GenericSpeedTests/Clamp.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Structure.Sketching.Benchmarks.GenericSpeedTests
+{
+    public class Clamp
+    {
+        [Benchmark(Description = "All Ternary Clamp")]
+        public byte ClampAllTernary()
+        {
+            double value = 256;
+            return (byte)(value < 0 ? 0 : value > 255 ? 255 : value);
+        }
+
+        [Benchmark(Baseline = true, Description = "Maths Clamp")]
+        public byte ClampMaths()
+        {
+            double value = 256;
+            return (byte)Math.Min(Math.Max(0, value), 255);
+        }
+
+        [Benchmark(Description = "No Maths Clamp")]
+        public byte ClampNoMaths()
+        {
+            double value = 256;
+            value = (value > 255) ? 255 : value;
+            value = (value < 0) ? 0 : value;
+            return (byte)value;
+        }
+
+        [Benchmark(Description = "No Maths Clamp No Ternary")]
+        public byte ClampNoMathsNoTernary()
+        {
+            double value = 256;
+
+            if (value > 255)
+            {
+                return 255;
+            }
+
+            if (value < 0)
+            {
+                return 0;
+            }
+
+            return (byte)value;
+        }
+    }
+}
