e2f1e480cc5b5dd7666fcd0ecb8700cd20eb62a7
James Craig <none@none>
2016-10-31 12:42:53 -0400

    Testing issues from Travis CI errors.

--- NetCoreTest/Program.cs
+++ NetCoreTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Structure.Sketching;
+using Structure.Sketching.Colors;
 using System;
 using System.IO;
 
@@ -6,24 +7,25 @@ namespace NetCoreTest
 {
     public class Program
     {
+        private static void ImageWrite()
+        {
+            using (var stream = new MemoryStream(File.ReadAllBytes("example.png")))
+            {
+                var image = new Image(stream).Apply(new Structure.Sketching.Filters.Convolution.Emboss())
+                                             .Apply(new Structure.Sketching.Filters.Binary.AdaptiveThreshold(10, Color.White, Color.Black, 0.5f));
+                image.Save("result.bmp");
+                image.Save("result.png");
+                image.Save("result.jpg");
+                image.Save("result.gif");
+            }
+        }
+
         private static bool LegalImage()
         {
             using (var stream = new MemoryStream(File.ReadAllBytes("example.png")))
             {
                 var image = new Image(stream);
                 return image.Width > 0 && image.Height > 0;
-            }
-        }
-
-        private static void ImageWrite()
-        {
-            using (var stream = new MemoryStream(File.ReadAllBytes("example.png")))
-            {
-                var image = new Image(stream);
-                image.Save("result.bmp");
-                image.Save("result.png");
-                image.Save("result.jpg");
-                image.Save("result.gif");
             }
         }
 
